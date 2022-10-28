7dcc025ebe9fe09abe441c3b11b71d78affcc44b
Abshert <abdulaishasalihu@gmail.com>
2022-10-27 18:26:59 -0700

    Sixth C programming task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,34 @@
 #include "main.h"
-
 /**
  * print_number - prints an integer.
  * @n: input integer.
  * Return: no return.
  */
-
 void print_number(int n)
 {
-	unsigned int u, i;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		u = -n;
-		_putchar('-');
+		_putchar(45);
+		m = n * -1;
 	}
 	else
 	{
-		u = n;
+		m = n;
 	}
 
-	i = 1000000000;
-	do {
-		if (i <= u || i == 1)
-			_putchar(u / i % 10 + '0');
-		i /= 10;
-	} while (i != 0);
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
