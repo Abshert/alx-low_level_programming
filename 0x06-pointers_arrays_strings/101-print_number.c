cc3e20110436d366b277bd8c0197dcec426ae28c
Abshert <abdulaishasalihu@gmail.com>
2022-10-29 02:54:29 -0700

    Sixth C programming task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,41 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include "main.h"
 
+
 /**
- * main - generates keygen.
- * Return: 0 Always.
- * print_number - prints an integer.
- * @n: input integer.
- * void print_number(int n)
-	* unsigned int m, d, count;
- */
-int main(void)
+  * print_number - print numbers chars
+  * @n: integer params
+  * Return: 0
+ **/
+
 void print_number(int n)
 {
-	int r = 0, c = 0;
-	time_t t;
-	unsigned int m, d, count;
+	unsigned int n1;
 
+	n1 = n;
 	if (n < 0)
 	{
-@@ -23, 3 + 35, 18 @@
-		d /= 10;
-		count *= 10;
+		_putchar('-');
+		n1 = -n;
 	}
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	for (; count >= 1; count /= 10)
+	if (n1 / 10 != 0)
 	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-		_putchar(((m / count) % 10) + 48);
+		print_number(n1 / 10);
 	}
-	printf("%c\n", (2772 - c));
-	return (0);
+	_putchar((n1 % 10) + '0');
 }
+
+
